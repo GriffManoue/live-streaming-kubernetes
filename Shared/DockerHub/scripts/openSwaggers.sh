6d891b489dad6b47e3e383209c2d6c7900e1949a
Buga Péter <buga.peti@gmail.com>
2025-03-16 17:39:19 +0100

    Update Kubernetes deployment configurations and scripts for AnalyticsService and Nginx-RTMP
    - Changed image name for AnalyticsService from 'bugapeti/analytics-service:latest' to 'bugapeti/analyticsservice:latest'.
    - Updated environment variable for AnalyticsService from 'Production' to 'Development'.
    - Modified image name for Nginx-RTMP from 'bugapeti/nginx-rtmp:latest' to 'bugapeti/nginx-rtmp-k8s:latest'.
    - Adjusted port-forwarding settings in forward-all.sh for better service management.
    - Introduced new script openSwaggers.sh to streamline access to Swagger documentation for all services.

---
+++ Shared/DockerHub/scripts/openSwaggers.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+start http://localhost:8081/swagger 
+start http://localhost:8082/swagger 
+start http://localhost:8083/swagger 
+start http://localhost:8084/swagger 
+start http://localhost:8085/swagger 
+start http://localhost:8086/swagger
