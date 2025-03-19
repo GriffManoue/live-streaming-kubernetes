e6b7a50f454b641beb27c534ce9008d2b001e4fd
Buga Péter <buga.peti@gmail.com>
2025-03-19 19:03:41 +0100

    feat(client): add StreamPlayer component and update routing; include Videogular styles and dependencies

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { StreamPlayerComponent } from './features/stream-player/stream-player.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: StreamPlayerComponent,
+        title: 'Stream Player',
+    }
+];
---
+++ Client/src/app/features/stream-player/stream-player.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stream-player',
+  imports: [],
+  templateUrl: './stream-player.component.html',
+  styleUrl: './stream-player.component.scss'
+})
+export class StreamPlayerComponent {
+
+}
