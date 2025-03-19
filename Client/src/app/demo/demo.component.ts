c2b99defd180991a04f30eec6bf1bd577fe151b4
Buga Péter <buga.peti@gmail.com>
2025-03-19 22:39:59 +0100

    feat: integrate PrimeNG and Tailwind CSS, add demo component
    - Updated package.json to include PrimeNG and Tailwind CSS dependencies.
    - Refactored app.component.html to replace placeholder content with <app-demo> component.
    - Created DemoComponent with a button using PrimeNG.
    - Configured app.module.ts to provide PrimeNG and Tailwind CSS themes.
    - Added styles for Tailwind CSS in styles.scss.
    - Created unit tests for DemoComponent.

---
+++ Client/src/app/demo/demo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button'
+
+@Component({
+  selector: 'app-demo',
+  imports: [ButtonModule],
+  templateUrl: './demo.component.html',
+  styleUrl: './demo.component.scss'
+})
+export class DemoComponent {
+
+}
