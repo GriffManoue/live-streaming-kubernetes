e954471348732bedd1d1bd3dc422ae93f96ac85f
Buga Péter <buga.peti@gmail.com>
2025-03-19 15:42:05 +0100

    feat(client): remove About and Home components; add Header and Footer components

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
+  /*
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent }
+   */
 ];
