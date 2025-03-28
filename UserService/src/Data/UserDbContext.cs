bfaaa8339ea120e33aab9d1a7215e0294598b3bc
Buga Péter <buga.peti@gmail.com>
2025-03-28 08:16:12 +0100

    refactor: change UserDbContext to implement IDbContext for better abstraction

--- UserService/src/Data/UserDbContext.cs
+++ UserService/src/Data/UserDbContext.cs
@@ -3,7 +3,7 @@ using Shared.Models.Domain;
 
 namespace UserService.Data;
 
-public class UserDbContext : DbContext
+public class UserDbContext : IDbContext
 {
     public UserDbContext(DbContextOptions<UserDbContext> options)
         : base(options)
