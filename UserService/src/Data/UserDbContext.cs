ae0c509ead5a06416858c2f0850b825ecbe8a596
Buga Péter <buga.peti@gmail.com>
2025-03-28 08:08:07 +0100

    refactor: simplify UserDbContext by removing unused code and ensuring proper entity relationships

--- UserService/src/Data/UserDbContext.cs
+++ UserService/src/Data/UserDbContext.cs
@@ -1,34 +1,26 @@
 using Microsoft.EntityFrameworkCore;
-using Shared.Data;
 using Shared.Models.Domain;
 
 namespace UserService.Data;
 
-public class UserDbContext : BaseDbContext
+public class UserDbContext : DbContext
 {
-    public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+    public UserDbContext(DbContextOptions<UserDbContext> options)
+        : base(options)
     {
     }
-    
-    public DbSet<User> Users { get; set; } = null!;
-    
+
+    public DbSet<User> Users { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        
-        // Configure User entity
+
         modelBuilder.Entity<User>()
-            .HasIndex(u => u.Username)
-            .IsUnique();
-        
-        modelBuilder.Entity<User>()
-            .HasIndex(u => u.Email)
-            .IsUnique();
-        
-        // Configure User entity relationships
-        modelBuilder.Entity<User>()
-            .HasMany(u => u.Followers)
-            .WithMany(u => u.Following)
-            .UsingEntity(j => j.ToTable("UserFollowers"));
+            .HasOne(u => u.Stream)
+            .WithOne(s => s.User)
+            .HasForeignKey<LiveStream>(s => s.UserId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
