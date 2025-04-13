59a3b522e9a21d9860b14ba64e304c06d50ebb4e
Buga Péter <buga.peti@gmail.com>
2025-04-13 15:29:49 +0200

    fix: add EnumMember attributes to StreamCategory enum values for serialization

--- Shared/src/Models/Enums/StreamCategory.cs
+++ Shared/src/Models/Enums/StreamCategory.cs
@@ -1,21 +1,56 @@
-﻿namespace Shared.models.Enums;
+﻿using System.Text.Json.Serialization;
+using System.Runtime.Serialization;
 
+namespace Shared.models.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum StreamCategory
 {
+    [EnumMember(Value = "Gaming")]
     Gaming,
+    
+    [EnumMember(Value = "Music")]
     Music,
+    
+    [EnumMember(Value = "Sports")]
     Sports,
+    
+    [EnumMember(Value = "Art")]
     Art,
+    
+    [EnumMember(Value = "Cooking")]
     Cooking,
+    
+    [EnumMember(Value = "Education")]
     Education,
+    
+    [EnumMember(Value = "Travel")]
     Travel,
+    
+    [EnumMember(Value = "TalkShows")]
     TalkShows,
+    
+    [EnumMember(Value = "News")]
     News,
+    
+    [EnumMember(Value = "Technology")]
     Technology,
+    
+    [EnumMember(Value = "Science")]
     Science,
+    
+    [EnumMember(Value = "HealthAndFitness")]
     HealthAndFitness,
+    
+    [EnumMember(Value = "FashionAndBeauty")]
     FashionAndBeauty,
+    
+    [EnumMember(Value = "FoodAndDrink")]
     FoodAndDrink,
+    
+    [EnumMember(Value = "PetsAndAnimals")]
     PetsAndAnimals,
+    
+    [EnumMember(Value = "DiyAndCrafts")]
     DiyAndCrafts
 }
