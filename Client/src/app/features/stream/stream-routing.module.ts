7118482fb79070371b2b076b1636755db4b8baf8
Buga Péter <buga.peti@gmail.com>
2025-04-22 20:28:36 +0200

    fix: update stream routing path to include streamId parameter

--- Client/src/app/features/stream/stream-routing.module.ts
+++ Client/src/app/features/stream/stream-routing.module.ts
@@ -5,7 +5,7 @@ import { AuthGuard } from '../../guards/auth.guard';
 
 const routes: Routes = [
   {
-    path: '',
+    path: ':streamId',
     component: StreamComponent,
     canActivate: [AuthGuard]
   }
