0af1c454c49fa17efc9a420df5376b9ba82de692
Buga Péter <buga.peti@gmail.com>
2025-04-23 18:26:12 +0200

    feat: Update AuthInterceptor to use providedIn root for better dependency management

--- Client/src/app/services/auth-interceptor.service.ts
+++ Client/src/app/services/auth-interceptor.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // Debug: Log outgoing request
